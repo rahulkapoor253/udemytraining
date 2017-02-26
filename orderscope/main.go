e819778a0a497a8ed561c1b81758e0d6d5cf92bd
rahulkapoor253 <rahul253801@gmail.com>
2017-02-26 12:52:55 +0530

    Add files via upload

---
+++ orderscope/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Printf("%v \n",x)//cant find any x;
+	x := 10//here order of declaring matters alot;
+	y :="rahul kapoor"
+
+	fmt.Printf("%v \n",y)
+}
