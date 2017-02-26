e326848a489327448f0fc242894e0bc4d70013f2
rahulkapoor253 <rahul253801@gmail.com>
2017-02-26 12:34:12 +0530

    Add files via upload

---
+++ types/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	x := 10
+	y := "rahul"
+	z :=true
+	var temp int
+	var xyz string
+
+	fmt.Printf("%T\n",x)
+	fmt.Printf("%T\n",y)
+	fmt.Printf("%T\n",z)
+	fmt.Printf("%v\n",temp)
+	fmt.Printf("%v\n",xyz)
+
+}
